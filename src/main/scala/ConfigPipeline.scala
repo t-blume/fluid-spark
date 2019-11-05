7a812ee73a686532f594979341ed007621af4155
Till Blume <tbl@informatik.uni-kiel.de>
2019-11-05 17:36:45 +0100

    increased network timeout

--- src/main/scala/ConfigPipeline.scala
+++ src/main/scala/ConfigPipeline.scala
@@ -27,6 +27,7 @@ class ConfigPipeline(config: MyConfig) {
   val conf = new SparkConf().setAppName(config.getString(config.VARS.spark_name)).
     setMaster(config.getString(config.VARS.spark_master)).
     set("spark.executor.heartbeatInterval", "10000s").
+    set("spark.network.timeout", "10000s").
     set("spark.eventLog.enabled", "true").
     set("spark.eventLog.dir", config.getString(config.VARS.spark_log_dir)).
     set("spark.driver.memory", maxMemory).
